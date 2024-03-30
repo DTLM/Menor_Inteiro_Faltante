9c41a05be949591a116cc18501d5a7d9f3233964
Thiago Martins <thiagolealmartins@gmail.com>
2024-03-30 20:08:16 -0300

    corrigindo alguns problemas

--- src/main/java/codility/main/Main.java
+++ src/main/java/codility/main/Main.java
@@ -12,23 +12,34 @@ public class Main {
 		int vetor [] = {1,2,3};
 		int vetor2 [] = {1,3,4,5};
 		int vetor3 [] = {1,2,3,8,6,7,5};
+		int vetor4 [] = {1};
+		int vetor5 [] = {4, 1, 5, 6, 2};
 		int resultado = solution(vetor);
 		int resultado2= solution(vetor2);
 		int resultado3 = solution(vetor3);
+		int resultado4 = solution(vetor4);
+		int resultado5 = solution(vetor5);
 		System.out.println(resultado);
 		System.out.println(resultado2);
 		System.out.println(resultado3);
+		System.out.println(resultado4);
+		System.out.println(resultado5);
 	}
 	
 	public static int solution(int[] A) {
-		// Implement your solution here
-        if(A == null || A.length <= 0) {
+		if(A == null || A.length <= 0) {
 			return 1;
 		}
-		List<Integer> lista = Arrays.stream(A).boxed().collect(Collectors.toList());
-		lista = lista.stream().filter(num -> num > 0).distinct().sorted((num1, num2) -> num1.compareTo(num2)).collect(Collectors.toList());
-		int result = 1;
-		for(int i = 0; i < lista.size() - 1; i++) {
+        List<Integer> lista = Arrays.stream(A).boxed().collect(Collectors.toList());
+        lista = lista.stream().filter(num -> num > 0).distinct().sorted((num1, num2) -> num1.compareTo(num2)).collect(Collectors.toList());
+        if(lista == null || lista.size() <= 0 || lista.get(0) != 1) {
+			return 1;
+		}
+        if(lista.size() == 1 && lista.get(0) == 1) {
+        	return 2;
+        }
+        int result = 1;
+        for(int i = 0; i < lista.size() - 1; i++) {
 			if(lista.get(i+1) - 1 != lista.get(i)) {
 				result = lista.get(i) + 1;
 				break;
